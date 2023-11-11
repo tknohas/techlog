053d6366dafdb7ffbfbf9c769d070d38841430cb
{tknohas} <{taka462411@gmail.com}>
2023-11-12 00:31:22 +0900

    deviseのviewファイル(編集用)を生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#top'
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
