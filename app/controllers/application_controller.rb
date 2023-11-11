ba202f1d78718ff8de81a675f0ba0d3ed9bfe25e
{tknohas} <{taka462411@gmail.com}>
2023-11-11 23:46:18 +0900

    devise導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_patameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_patameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
