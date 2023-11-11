f740b66bde1c45d2986f2653ab9fdfad1f25be7a
{tknohas} <{taka462411@gmail.com}>
2023-11-11 22:52:40 +0900

    アセットファイル等はrails generate時に自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module TechlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g| # ここから追記
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper     false      # helperファイルを自動生成しない
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
